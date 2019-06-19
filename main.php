27bc9603f2c669d87dbe7dcbbe9121c51934c36c
gobtech <gobtech@hotmail.com>
2019-06-18 17:55:59 -0700

    Update main.php

--- main.php
+++ main.php
@@ -6,3 +6,4 @@
 <p><font size ="4.5">Resources: </p><a href="resourceView.php"><font size= "3">Click Here to see all AMS resources</a><br/>
 <p><font size ="4.5">Business: </p><a href="businessView.php"><font size= "3">Click Here to see all AMS-run businesses in UBC</a><br/>
 <p><font size ="4.5">Locations: </p><a href="locationView.php"><font size= "3">Click Here to view all AMS owned locations around campus</a><br/>
+<p><font size ="4.5">Locations: </p><a href="bookingreservesView.php"><font size= "3">Click here to view all booking reservations</a><br/>
